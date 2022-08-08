55e887d6e450f27632dccd3cbbf7a9f99b5029ee
= <=>
2022-08-08 21:55:54 +0000

    Model updated

--- products_api/models.py
+++ products_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Products(models.Model):
     imgName  = models.CharField(max_length=100)
-    tag = models.CharField(max_length=100)
+    tag = models.CharField(max_length=100, default="technology")
     title = models.CharField(max_length=100)
     price = models.DecimalField(decimal_places=2, max_digits=100000, default="100.20")
 
